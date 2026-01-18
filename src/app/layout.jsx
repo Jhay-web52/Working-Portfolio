e7019813687480d25cb88ee0f294fc54f58155fa
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-01-18 23:05:31 +0530

    classname issue solved in vercel deployment

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import ClientLoaderCleanup from "@/components/ClientLoaderCleanup"
+import ClientLoaderCleanup from "@/components/ClientLoaderCleanup";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,51 +19,61 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ backgroundColor: "#121212" }}>
+        {/* inintial loading effct using only css */}
+        <div id="initial-loader">
+          {/* <!-- Floating particles --> */}
+          <div className="loader-particle"></div>
+          <div className="loader-particle"></div>
+          <div className="loader-particle"></div>
+          <div className="loader-particle"></div>
+          <div className="loader-particle"></div>
+          <div className="loader-particle"></div>
 
-{/* inintial loading effct using only css */}
-<div id="initial-loader">
-  {/* <!-- Floating particles --> */}
-  <div class="loader-particle"></div>
-  <div class="loader-particle"></div>
-  <div class="loader-particle"></div>
-  <div class="loader-particle"></div>
-  <div class="loader-particle"></div>
-  <div class="loader-particle"></div>
-
- <div class="loader-content">
-    {/* <!-- Typing animation --> */}
-    <div class="typing-container">
-      <div class="typing-line">
-        <span class="typing-text typing-text-1">Welcome to Prabhulal's Portfolio ⚡</span>
-      </div>
-      <div class="typing-line">
-        <span class="typing-text typing-text-2">Front-end Developer & Designer 💻</span>
-      </div>
-      <div class="typing-line">
-        <span class="typing-text typing-text-3">Please Wait, Loading Your Experience ✨</span>
-      </div>
-      <div class="typing-line">
-        <span class="typing-text typing-text-4">Preparing Something Special For You 🎨</span>
-      </div>
-      <div class="typing-line">
-        <span class="typing-text typing-text-5">Almost There, Stay Tuned 🚀</span>
-      </div>
-      <div class="typing-line">
-        <span class="typing-text typing-text-6">Thanks For Your Patience 💜</span>
-      </div>
-      <div class="typing-line">
-        <span class="typing-text typing-text-7">Get Ready For An Amazing Journey 🌟</span>
-      </div>
-    </div>
-  </div>
-</div>
-
-
+          <div class="loader-content">
+            {/* <!-- Typing animation --> */}
+            <div className="typing-container">
+              <div className="typing-line">
+                <span className="typing-text typing-text-1">
+                  Welcome to Prabhulal's Portfolio ⚡
+                </span>
+              </div>
+              <div className="typing-line">
+                <span className="typing-text typing-text-2">
+                  Front-end Developer & Designer 💻
+                </span>
+              </div>
+              <div className="typing-line">
+                <span className="typing-text typing-text-3">
+                  Please Wait, Loading Your Experience ✨
+                </span>
+              </div>
+              <div className="typing-line">
+                <span className="typing-text typing-text-4">
+                  Preparing Something Special For You 🎨
+                </span>
+              </div>
+              <div className="typing-line">
+                <span className="typing-text typing-text-5">
+                  Almost There, Stay Tuned 🚀
+                </span>
+              </div>
+              <div className="typing-line">
+                <span className="typing-text typing-text-6">
+                  Thanks For Your Patience 💜
+                </span>
+              </div>
+              <div className="typing-line">
+                <span className="typing-text typing-text-7">
+                  Get Ready For An Amazing Journey 🌟
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* Removes CSS loader after hydration */}
         <ClientLoaderCleanup />
